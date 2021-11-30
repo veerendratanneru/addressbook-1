c352d21ac98cc798431d28473679f5dd9e82f656
b <l>
2021-11-30 22:14:50 +0530

    added sh file

--- prepare-ACM.sh
+++ prepare-ACM.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
-pip3 install ansible -y
+pip3 install ansible
 ansible --version
-pip3 install boto3 -y
+pip3 install boto3
 ansible-inventory inventory_aws_ec2.yml --list
 ansible-playbook docker-deploy.yml
 rm /home/ec2_user/.ssh/id_rsa -y
