6f7f5076128cedc3cc34e407a5882b3115dcefd0
b <l>
2021-12-01 05:57:39 +0530

    changes

--- prepare-ACM.sh
+++ prepare-ACM.sh
@@ -2,6 +2,6 @@
 pip3 install ansible
 ansible --version
 pip3 install boto3
-ansible-inventory inventory_aws_ec2.yml --list
+ansible-inventory -i inventory_aws_ec2.yml --list
 ansible-playbook docker-deploy.yml
 rm /home/ec2_user/.ssh/id_rsa -y
