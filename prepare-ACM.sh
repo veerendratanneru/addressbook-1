8852e87b87832508f37589a1da2ef53daff14daf
b <l>
2021-12-01 06:30:25 +0530

    changes

--- prepare-ACM.sh
+++ prepare-ACM.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 pip3 install ansible==2.10.7
 whoami
-ansible --version
+/home/ec2-user/.local/bin/ansible --version
 pip3 install boto3
 ansible-inventory -i inventory_aws_ec2.yml --list
 ansible-playbook docker-deploy.yml
