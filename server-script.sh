119d50c051b2bf92222ef84a55ec69467547b55c
b <l>
2021-12-28 09:33:15 +0530

    ssh agent added

--- server-script.sh
+++ server-script.sh
@@ -1,7 +1,7 @@
 sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
 sudo yum install maven -y
-if [-d "addressbook-1"]
+if [-e "addressbook-1"]
 then
   echo "repo is cloned"
 else
