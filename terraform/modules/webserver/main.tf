88fb03abcffe2e803bf32f2f8744d73dd0947722
b <l>
2022-01-19 19:54:31 +0530

    added

--- terraform/modules/webserver/main.tf
+++ terraform/modules/webserver/main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "webserver" {
   associate_public_ip_address = true
   subnet_id = var.subnet_id
   vpc_security_group_ids = [aws_security_group.mywebsecurity.id]
-  key_name="ansiblekey"
+  key_name="ansible-key"
   user_data =file("server-script.sh")
  
   tags={
